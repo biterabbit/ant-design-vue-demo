1ff50e4918207526cfcfa46c9e1d41259137f18a
zhengxiafeng <1312335750@qq.com>
2020-01-13 10:21:27 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <a-tree
+    :treeData="treeData"
+    showIcon
+    defaultExpandAll
+    :defaultSelectedKeys="['0-0-0']"
+  >
+    <a-icon type="down" slot="switcherIcon" />
+    <a-icon slot="smile" type="smile-o" />
+    <a-icon slot="meh" type="smile-o" />
+    <template slot="custom" slot-scope="{ icon }">
+      <a-icon :type="icon" />-{{icon}}-
+    </template>
+  </a-tree>
+</template>
+<script>
+const treeData = [
+  {
+    title: "parent 1",
+    key: "0-0",
+    slots: {
+      icon: "smile"
+    },
+    children: [
+      { title: "leaf", key: "0-0-0", icon: 'frown', slots: { icon: "meh" } },
+      { title: "leaf", key: "0-0-1", icon: 'frown-o', scopedSlots: { icon: "custom" } }
+    ]
+  }
+];
+
+export default {
+  data() {
+    return {
+      treeData
+    };
+  },
+  methods: {
+    onSelect(selectedKeys, info) {
+      console.log("selected", selectedKeys, info);
+    },
+    onCheck(checkedKeys, info) {
+      console.log("onCheck", checkedKeys, info);
+    }
+  }
+};
+</script>
